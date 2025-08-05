1c786762065bbd13671bd3574dbb49718a9104d9
adtheus <adrien.theus@gmail.com>
2025-08-05 13:53:10 +0200

    ajout script de creation de db

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const url = configService.get<string>('DATABASE_URL');
+        console.log('DB URL used by TypeORM:', url);
+        return {
+          type: 'mysql',
+          url,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          cache: false,
+          synchronize: true,
+        };
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
