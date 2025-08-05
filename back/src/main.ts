fb4f2e6c613a2a63e242d397ca31bbabc051be7f
adtheus <adrien.theus@gmail.com>
2025-08-05 13:11:14 +0200

    dockerfile: back front

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  const config = new DocumentBuilder()
+  .setTitle('Radio I/O')
+  .setDescription('Description des endpoints de l\'API')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  var cors = require('cors');
+  var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, 
+    credentials : true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'] // pour MAC sinon CORS (pas sur que AUthorization soit nécessaire)
+  }
+  app.use(cors( corsOptions ));
+  app.use(cookieParser());
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
