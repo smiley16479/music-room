cd2f157cc3c233e73766838f0b9dbde759200770
adtheus <adrien.theus@gmail.com>
2025-08-06 17:59:55 +0200

    désactivation des MailModule

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,20 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { EventModule } from './event/event.module';
-import { PlaylistModule } from './playlist/playlist.module';
-import { DeviceModule } from './device/device.module';
-import { InvitationModule } from './invitation/invitation.module';
-// import { DatabaseModule } from './database/database.module';
-// import { EmailModule } from './email/email.module';
-import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { MusicModule } from './music/music.module';
+import { DeviceModule } from './device/device.module';
+// import { EmailModule } from './email/email.module';
+// import { MailModule } from './mail/mail.module';
+// import { EventModule } from './event/event.module';
+// import { PlaylistModule } from './playlist/playlist.module';
+// import { InvitationModule } from './invitation/invitation.module';
 import { APP_GUARD, APP_FILTER } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { AuthExceptionFilter } from './auth/filters/auth-exception.filter';
-import { MusicModule } from './music/music.module';
-
 
 @Module({
   imports: [
@@ -38,29 +37,29 @@ import { MusicModule } from './music/music.module';
         };
       }
     }),
-    UserModule,
-    EventModule,
-    PlaylistModule,
-    DeviceModule,
-    InvitationModule,
-    // DatabaseModule,
-    // EmailModule,
-    MailModule,
     AuthModule,
+    UserModule,
+    DatabaseModule,
     MusicModule,
+    DeviceModule,
+    // EmailModule,
+    // MailModule,
+    // EventModule,
+    // PlaylistModule,
+    // InvitationModule,
   ],
   controllers: [AppController],
   providers: [AppService,
-    // Global JWT Guard
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-    // Global Auth Exception Filter
-    {
-      provide: APP_FILTER,
-      useClass: AuthExceptionFilter,
-    },
+    // // Global JWT Guard
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: JwtAuthGuard,
+    // },
+    // // Global Auth Exception Filter
+    // {
+    //   provide: APP_FILTER,
+    //   useClass: AuthExceptionFilter,
+    // },
   ],
 })
 export class AppModule {}
